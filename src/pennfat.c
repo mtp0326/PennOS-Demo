04c367816833209b38048f2b5f5bfcce6c156b87
Aaron <aaront05@seas.upenn.edu>
2024-04-10 16:31:32 -0400

    mount and unmount fixes

--- src/pennfat.c
+++ src/pennfat.c
@@ -113,6 +113,7 @@ void mkfs(const char* fs_name, int blocks_in_fat, int block_size_config) {
     perror("write error");
     // exit(EXIT_FAILURE);
   }
+  close(fs_fd);
 }
 
 int mount(const char* fs_name) {
@@ -180,6 +181,7 @@ int unmount() {
     return -1;
     // exit(EXIT_FAILURE);
   }
+  close(fs_fd);
   fat = NULL;
   return 0;
 }
